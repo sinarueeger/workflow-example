93b3f711da496f188d77159f5c4fed642c98890f
Sina Rueeger <sina.rueeger@gmail.com>
2018-12-09 21:32:54 +0100

    fix dependencies

--- tidytuesday-with-drake/make.R
+++ tidytuesday-with-drake/make.R
@@ -14,38 +14,36 @@
 
 source(here::here("src", "setup.R"))
 source(here::here("src", "functions.R"))
+## download data -----------
+download.file("https://raw.github.com/rfordatascience/tidytuesday/master/data/2018-09-11/cats_vs_dogs.csv", "cats_vs_dogs.csv")
 
 ##--------------
 ##  plan     --
 ##--------------
 
 plan <- drake_plan(
-  
-  ## download data -----------
-  download_data = download.file("https://raw.github.com/rfordatascience/tidytuesday/master/data/2018-09-11/cats_vs_dogs.csv", here::here("data", "cats_vs_dogs.csv")),
- 
   ## read in data ------------
-  dat = readr::read_csv(here::here("data", "cats_vs_dogs.csv")) %>% mutate(state = tolower(state)),
+  raw_data = readr::read_csv(file_in("cats_vs_dogs.csv")) %>% mutate(state = tolower(state)),
   
   ## transform data ----------
-  dat_added = dat %>% mutate(ratio_avg_per_household = avg_cats_per_household/avg_dogs_per_household, ratio_population = cat_population/dog_population),
+  data = raw_data %>% mutate(ratio_avg_per_household = avg_cats_per_household/avg_dogs_per_household, ratio_population = cat_population/dog_population),
   
   ## get coordinates ---------
   coord_states = ggplot2::map_data("state") %>% rename(state = region),
   
   ## add geo coordinates for each state
-  dat_with_states = left_join(dat_added, coord_states %>% select(-subregion), by = "state"),
+  data_with_states = left_join(data, coord_states %>% select(-subregion), by = "state"),
   
   ## summarise ---------------
-  summary_cats_and_dogs = skimr::skim(dat_added %>% select(state, n_households:ratio_population)),
+  summary_cats_and_dogs = skimr::skim(data %>% select(state, n_households:ratio_population)),
   
   ## plots -------------------
   
-  plots_cats_and_dogs = purrr::map(dat_with_states %>% select(n_households:ratio_population) %>% names(),  ~map_cats_and_dogs(., dat_with_states)),
+  plots_cats_and_dogs = purrr::map(data_with_states %>% select(n_households:ratio_population) %>% names(),  ~map_cats_and_dogs(., data_with_states)),
  
   ## report ------------------ 
   report = rmarkdown::render(
-    knitr_in(here::here("report", "report.Rmd")),
+    knitr_in("report.Rmd"),
     output_file = file_out("report.html"),
     quiet = TRUE
   )
@@ -53,9 +51,9 @@ plan <- drake_plan(
 
 
 
-##--------------
-##  make     --
-##--------------
+##------------------------
+##  drake functions     --
+##------------------------
 
 
 config <- drake_config(plan) 
@@ -68,3 +66,5 @@ make(plan)
 config <- drake_config(plan) 
 vis_drake_graph(config) 
 
+## remove results
+clean()
