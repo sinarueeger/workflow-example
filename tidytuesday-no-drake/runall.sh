4de32cbbd626494b89f5fb18e2d68d8cf71d676a
Sina Rueeger <sina.rueeger@gmail.com>
2018-12-10 07:13:38 +0100

    updating readme

--- tidytuesday-no-drake/runall.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-Rscript --vanilla src/analysis.R
-Rscript --vanilla -e "rmarkdown::render(\"report/report.Rmd\")"
--- tidytuesday-no-drake/src/setup.R
+++
@@ -1,7 +0,0 @@
-
-library(ggplot2)
-library(magrittr)
-library(readr)
-library(dplyr)
-library(stringr)
-library(purrr)
